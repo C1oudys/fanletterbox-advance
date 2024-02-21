a20e926daa2224485785ddaafc3674761b64c5ab
C1oudys <hymin310@gmail.com>
2024-02-22 02:02:22 +0900

    fix: 새로고침시 로그인유지되게 fix

--- src/redux/modules/authSlice.js
+++ src/redux/modules/authSlice.js
@@ -19,13 +19,9 @@ const authSlice = createSlice({
       state.userId = userId;
       state.nickname = nickname;
       state.avatar = avatar;
-      //   console.log(`id: ${state.userId}`);
-      //   console.log("accessToken>", accessToken);
-      //   console.log(`nickname: ${state.nickname}`);
-      //   console.log(`avatar: ${state.avatar}`);
-      localStorage.setItem("nickname", JSON.stringify(nickname));
       localStorage.setItem("accessToken", accessToken);
       localStorage.setItem("userId", JSON.stringify(userId));
+      localStorage.setItem("nickname", JSON.stringify(nickname));
       localStorage.setItem("avatar", JSON.stringify(avatar));
     },
 
@@ -35,10 +31,27 @@ const authSlice = createSlice({
       state.userId = "";
       state.nickname = "";
       state.avatar = "";
-      localStorage.removeItem("response");
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("userId");
+      localStorage.removeItem("nickname");
+      localStorage.removeItem("avatar");
+    },
+
+    initUser: (state) => {
+      const accessToken = localStorage.getItem("accessToken");
+      const userId = JSON.parse(localStorage.getItem("userId"));
+      const nickname = JSON.parse(localStorage.getItem("nickname"));
+      const avatar = JSON.parse(localStorage.getItem("avatar"));
+      if (accessToken && userId && nickname && avatar) {
+        state.isLoggedIn = true;
+        state.accessToken = accessToken;
+        state.userId = userId;
+        state.nickname = nickname;
+        state.avatar = avatar;
+      }
     },
   },
 });
 
+export const { userLogin, userLogout, initUser } = authSlice.actions;
 export default authSlice.reducer;
-export const { userLogin, userLogout } = authSlice.actions;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { initUser } from '../redux/modules/authSlice';
 import Detail from '../pages/Detail';
 import Home from '../pages/Home';
-import Login from '../pages/Login'; 
-import { useSelector } from 'react-redux';
+import Login from '../pages/Login';
 import Layout from '../components/Layout';
 import Profile from '../pages/Profile';
 
 export default function Router() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initUser());
+  }, [dispatch]);
 
   return (
     <BrowserRouter>
@@ -17,7 +23,7 @@ export default function Router() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/detail/:id" element={<Detail />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={isLoggedIn ? <Profile /> : <Navigate replace to="/login" />} />
         </Route>
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
